98e062384880a5d11b275e78d943fc9d3268b370
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2023-07-13 16:07:29 -0400

    Update Main.java
    Fixed syntax errors from Replit

--- Main.java
+++ Main.java
@@ -5,23 +5,21 @@ class Main {
     System.out.println("Hello World!");
     System.out.println("Board Class Testing START");   
     //Place Code to Test Board Class Here:
-    Board gameBoard = new Board();
+    Board gameBoard = new Board(null, null);
 
 
-    char[][] gb = new char [30][24];
+    char[][] gb = new char[30][24];
 
     for(int i = 0; i < 30; i+= 3){
-      for(int j = 0; j < 24; j += 3{
+      for(int j = 0; j < 24; j += 3) {
         
       }
     System.out.println("Board Class Testing END");
     System.out.println("Point Class Testing START");
     //Place Code to Test Point Class Here:
-    Point testPoint = new Point();
+    Point testPoint = new Point(i, i);
       
     System.out.println("Point Class Testing END");
-      
-
     }
-  
+  }
 }
